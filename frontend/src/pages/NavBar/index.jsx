72a3fbd86f4d616c75a8e8c152156cfa42e06021
Kaleab Abayneh <92297934+itu-itis21-gizaw21@users.noreply.github.com>
2023-09-29 15:28:15 +0300

    Update index.jsx

--- frontend/src/pages/NavBar/index.jsx
+++ frontend/src/pages/NavBar/index.jsx
@@ -67,7 +67,7 @@ const NavBar = (props) => {
                     },
                 }}
                 >
-                ITUraf.eth
+                ITUraf
             </Typography>
         </FlexAround>
        {isNonMobileScreen &&
