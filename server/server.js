4445662263f24fc3dc680704fea2bb18dc95f56c
Kaleab Abayneh <92297934+itu-itis21-gizaw21@users.noreply.github.com>
2023-10-16 20:13:34 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -85,6 +85,7 @@ app.patch('/api/uposts', async (req, res) => {
        const posts = await Post.find()
           
       const postsx = await Post.find()
+           .sort({ createdAt: -1 })
            .limit(1);
         let op = postsx[0].numbers;
        /*
