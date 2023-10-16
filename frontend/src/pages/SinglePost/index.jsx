cb35be1bb681d65e59c341eae1bbc0f916cd89e9
Kaleab Abayneh <92297934+itu-itis21-gizaw21@users.noreply.github.com>
2023-10-16 22:55:32 +0300

    Update index.jsx

--- frontend/src/pages/SinglePost/index.jsx
+++ frontend/src/pages/SinglePost/index.jsx
@@ -60,7 +60,7 @@ const SinglePost = (props) => {
     };
 
     fetchData();
-  }, [page, dispatch]);
+  }, []);
   const posts = useSelector((state) => state.posts);
   const selectedPost = posts.find((post) => post._id === postId);
   if (!selectedPost) return null;
