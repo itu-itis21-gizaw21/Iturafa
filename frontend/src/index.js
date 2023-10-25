d2ac8b095b5087b6e645897096e22d2472df995a
Kaleab Abayneh <92297934+itu-itis21-gizaw21@users.noreply.github.com>
2023-10-25 20:30:07 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -83,9 +83,9 @@ async function GfetchUserData(createdX){
 }
 
 let uniqueIdentifier = localStorage.getItem('uniqueIdentifier');
-//let reloadx = localStorage.getItem('reloadx') || false;
-//if(!reloadx){
- // console.log("Yaaap");
+let reloadx = localStorage.getItem('reloadx') || false;
+if(!reloadx){
+   console.log("Yaaap");
   let reloadx = localStorage.setItem('reloadx','1');
 //}
 if (uniqueIdentifier) {
